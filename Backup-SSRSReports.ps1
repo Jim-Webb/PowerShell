5d1bfeabe6570b8c80033f1ed9f29dcfe018b7fd
Jim-Webb <jwebb2@gmail.com>
2022-01-27 13:13:57 -0500

    Update

--- Backup-SSRSReports.ps1
+++ Backup-SSRSReports.ps1
@@ -5,9 +5,13 @@ Param (
     [string]$BackupDirectory
 )
 
+# The script was taken from the blog post below. The site is no longer available.
+# http://www.sqlmusings.com/2011/03/28/how-to-download-all-your-ssrs-report-definitions-rdl-files-using-powershell/
+
+# This is an article by Garth Jones that covers the use of this script.
 # https://www.recastsoftware.com/resources/how-do-you-backup-all-of-your-custom-configmgr-reports/
 
-#note this is tested on PowerShell v2 and SSRS 2008 R2
+# note this is tested on PowerShell v2 and SSRS 2008 R2
 [void][System.Reflection.Assembly]::LoadWithPartialName("System.Xml.XmlDocument");
 [void][System.Reflection.Assembly]::LoadWithPartialName("System.IO");
  
@@ -19,9 +23,9 @@ If (!($ReportServer))
 $ReportServerUri = "https://$ReportServer/ReportServer/ReportService2005.asmx";
 $Proxy = New-WebServiceProxy -Uri $ReportServerUri -Namespace SSRS.ReportingService2005 -UseDefaultCredential ;
  
-#check out all members of $Proxy
-#$Proxy | Get-Member
-#http://msdn.microsoft.com/en-us/library/aa225878(v=SQL.80).aspx
+# check out all members of $Proxy
+# $Proxy | Get-Member
+# http://msdn.microsoft.com/en-us/library/aa225878(v=SQL.80).aspx
  
 #second parameter means recursive
 $items = $Proxy.ListChildren("/", $true) | `
@@ -29,10 +33,10 @@ $items = $Proxy.ListChildren("/", $true) | `
          Where-Object {$_.type -eq "Report"};
  
 $Count = ($items.count)         
-#create a new folder where we will save the files
-#PowerShell datetime format codes http://technet.microsoft.com/en-us/library/ee692801.aspx
+# create a new folder where we will save the files
+# PowerShell datetime format codes http://technet.microsoft.com/en-us/library/ee692801.aspx
  
-#create a timestamped folder, format similar to 2011-Mar-28-0850PM
+# create a timestamped folder, format similar to 2011-Mar-28-0850PM
 $folderName = Get-Date -format "yyyy-MMM-dd-hhmmtt";
 If ($BackupDirectory)
 {
@@ -73,15 +77,14 @@ foreach($item in $items)
     [byte[]] $reportDefinition = $null;
     $reportDefinition = $Proxy.GetReportDefinition($item.Path);
  
-    #note here we're forcing the actual definition to be 
-    #stored as a byte array
-    #if you take out the @() from the MemoryStream constructor, you'll 
-    #get an error
+    # note here we're forcing the actual definition to be 
+    # stored as a byte array
+    # if you take out the @() from the MemoryStream constructor, you'll 
+    # get an error
     [System.IO.MemoryStream] $memStream = New-Object System.IO.MemoryStream(@(,$reportDefinition));
     $rdlFile.Load($memStream);
  
     $fullReportFileName = $fullSubfolderName + "\" + $item.Name +  ".rdl";
     #Write-Host $fullReportFileName;
     $rdlFile.Save( $fullReportFileName);
- 
 }
