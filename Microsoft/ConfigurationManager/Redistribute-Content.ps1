488a2605af9b3302c6440cf84eeecf8d789fa8e7
Jim-Webb <james.webb@nationwidechildrens.org>
2025-02-16 14:34:34 -0500

    Create Redistribute-Content.ps1

---
+++ Microsoft/ConfigurationManager/Redistribute-Content.ps1
@@ -0,0 +1,31 @@
+﻿Function Redistribute-Content { 
+    [CMDletBinding()] 
+    param ( 
+    [Parameter(Mandatory=$True)] 
+    [ValidateNotNullorEmpty()] 
+    [String]$DistributionPoint, 
+    [Parameter()] 
+    [ValidateNotNullorEmpty()] 
+    [String]$SiteCode = 'PS1',
+    [Parameter()] 
+    [ValidateNotNullorEmpty()] 
+    [String]$PrimaryServer = 'CM01'
+    ) 
+    Process {
+    Write-Host "Site server: $PrimaryServer"
+    Write-Host "Site code: $SiteCode"
+    $query = 'SELECT * FROM SMS_PackageStatusDistPointsSummarizer WHERE State = 2 OR State = 3' 
+    $Packages = Get-WmiObject -ComputerName $PrimaryServer -Namespace "root\SMS\Site_$($SiteCode)" -Query $query | Select-Object PackageID, @{N='DistributionPoint';E={$_.ServerNalPath.split('\')[2]}} 
+    $FailedPackages = $Packages | Where-Object {$_.DistributionPoint -like "$DistributionPoint*"} | Select-Object -ExpandProperty PackageID
+
+    Write-Host "Packages that failed to distribute to $DistributionPoint. `n $FailedPackages."
+
+    foreach ($PackageID in $FailedPackages) { 
+        $List = Get-WmiObject -ComputerName $PrimaryServer -Namespace "root\SMS\Site_$($SiteCode)" -Query "Select * From SMS_DistributionPoint WHERE PackageID='$PackageID' AND ServerNALPath like '%$DistributionPoint%'"
+        Write-Host "Refreshing package $PackageID on $DistributionPoint."
+        $List.RefreshNow = $True 
+        $List.Put() | Out-Null
+        } 
+    } 
+}
+
