b6099d12de30ffa9c2246c6eb8fb2db73c0ba99c
Jim-Webb <james.webb@nationwidechildrens.org>
2025-02-16 14:34:14 -0500

    Create CollectionsInAFolder.ps1

---
+++ Microsoft/ConfigurationManager/CollectionsInAFolder.ps1
@@ -0,0 +1,6 @@
+﻿$FolderID = 16779269 # Use the WMI tool to get the FolderID
+$CollectionsInSpecficFolder = Get-WmiObject -ComputerName CMServer -Namespace "ROOT\SMS\Site_HOS" -Query "select * from SMS_Collection where CollectionID is in(select InstanceKey from SMS_ObjectContainerItem where ObjectType='5000' and ContainerNodeID='$FolderID') and CollectionType='2'"
+
+$SelectedFolder = $CollectionsInSpecficFolder | Select-Object Name, CollectionID, MemberCount | Sort-Object Name | Out-GridView -Title "Select the destination collection:" -OutputMode Single
+
+$CollectionsInSpecficFolder.Name
