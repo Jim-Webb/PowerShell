f182d4705cbeff4744607c54ea96f679688d49f8
Jim-Webb <james.webb@nationwidechildrens.org>
2025-02-22 19:10:41 -0500

    Create ChangeApplicationsInFolder-SendToProtectedDistributionPoint.ps1

---
+++ Microsoft/ConfigurationManager/ChangeApplicationsInFolder-SendToProtectedDistributionPoint.ps1
@@ -0,0 +1,13 @@
+﻿[string]$SiteCode = "PS1"
+[string]$SiteServer = 'CM01.corp.viamonstra.com'
+
+Application\PatchMyPC
+
+$Applications = (Get-WmiObject -Namespace "ROOT\SMS\Site_$Sitecode" -ComputerName $SiteServer -Query "select * from SMS_ApplicationLatest where ObjectPath = '/CrowdStrike'").LocalizedDisplayName
+
+Foreach ($Application in $Applications)
+{
+    Set-CMApplication -Name $Application -SendToProtectedDistributionPoint $true
+}
+
+(Get-WmiObject -Namespace "ROOT\SMS\Site_$Sitecode" -ComputerName $SiteServer -Query "select * from SMS_Package where ObjectPath = '/Microsoft'").Name
