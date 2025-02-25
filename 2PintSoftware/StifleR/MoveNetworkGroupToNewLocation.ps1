ed356f7573cb7e97e2d16347da632a53998cba0a
Jim-Webb <james.webb@nationwidechildrens.org>
2025-02-25 08:17:56 -0500

    Create MoveNetworkGroupToNewLocation.ps1

---
+++ 2PintSoftware/StifleR/MoveNetworkGroupToNewLocation.ps1
@@ -0,0 +1,6 @@
+﻿$NetworkGroupName = 'New York Office Building'
+$NewLocation = "1c20b999-7ca3-4c3a-ae89-e7fc7315da4d"
+
+$NetworkGroup = Get-WmiObject -Class NetworkGroups -Namespace root\stifler | where {$_.Name -eq $NetworkGroupName}
+
+$NetworkGroup.MoveToNewLocation($NewLocation)
