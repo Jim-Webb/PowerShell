1160d7a15e91fabb9c528b50873c8ab56921af86
Jim-Webb <james.webb@nationwidechildrens.org>
2025-02-25 08:16:34 -0500

    Create MoveNetworkToNewNetworkGroup.ps1

---
+++ 2PintSoftware/StifleR/MoveNetworkToNewNetworkGroup.ps1
@@ -0,0 +1,13 @@
+﻿$Subnet = '192.168.5.0'
+$NetworkGroupName = 'VPN Clients'
+
+$NetworkGroup = Get-WmiObject -Class NetworkGroups -Namespace root\stifler | where {$_.Name -eq $NetworkGroupName}
+
+If ($NetworkGroupName)
+{
+    Write-Host "Moving subnet $Subnet to NetworkGroup $($NetworkGroup.Name)"
+    $Network = Get-WmiObject -Class Networks -Namespace root\stifler | where {$_.NetworkId -eq $Subnet}
+
+    $Network.MoveToNewNetworkGroup($($NetworkGroup.ID))
+}
+
